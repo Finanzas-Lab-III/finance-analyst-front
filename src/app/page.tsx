8cf746b1bc17240e94fe6402e11986d8025b6288
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2025-09-08 16:26:36 -0300

    feat: login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function Page() {
+  const userId = cookies().get('userId')?.value;
+  if (!userId) {
+    redirect('/login');
+  }
   redirect('/backoffice/archive');
 }
