4e83c46555d2154d4a41aeb78b35b17deb284b1d
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2025-09-28 21:10:24 -0300

    feat: change NEXT_PUBLIC_AZURE_AD_AUTHORITY to AZURE_AD_AUTHORITY

--- src/lib/msal.ts
+++ src/lib/msal.ts
@@ -4,7 +4,7 @@ import { Configuration, LogLevel, PublicClientApplication } from "@azure/msal-br
 
 // Expect these variables to be defined in .env (NEXT_PUBLIC_) for client usage
 const clientId = process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID as string;
-const envAuthority = process.env.NEXT_PUBLIC_AZURE_AD_AUTHORITY as string | undefined; // e.g. https://login.microsoftonline.com/<tenantId>
+const envAuthority = process.env.AZURE_AD_AUTHORITY as string | undefined; // e.g. https://login.microsoftonline.com/<tenantId>
 const tenantId = process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID as string | undefined; // alternative to authority
 const redirectUri = process.env.NEXT_PUBLIC_AZURE_AD_REDIRECT_URI as string | undefined; // e.g. https://localhost:3000/login
 
