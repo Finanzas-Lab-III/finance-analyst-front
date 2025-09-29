c956d733bf69817b4eb79ed01894de1c4add8c7e
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2025-09-29 10:50:32 -0300

    fix: I just don't know anymore what I am doing

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,16 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
+const BACKEND_BASE = process.env.NEXT_PUBLIC_BACKEND_BASE ?? "https://lilyanna-synclastic-homeostatically.ngrok-free.dev";
+
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', '*.vercel.app'],
-    },
-  },
   async rewrites() {
-    if (!process.env.NEXT_PUBLIC_SERVICE_URL) {
-      throw new Error('NEXT_PUBLIC_API_URL is not defined');
-    }
     return [
       {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_SERVICE_URL}/api/:path*`,
+        source: "/api/:path*",
+        destination: `${BACKEND_BASE}/api/:path*`,
       },
     ];
-  },
-  eslint: {
-    // Allow production builds to successfully complete even if there are ESLint errors
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Allow production builds to successfully complete even if there are type errors
-    ignoreBuildErrors: true,
   },
 };
 
